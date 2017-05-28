7da2c237224d4130cd35a653081a6457013c40b6
Ronan Versendaal <ronanversendaal@hotmail.com>
2017-05-28 20:38:27 +0200

    better seeder and styles

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -19,11 +19,13 @@ class UserTableSeeder extends Seeder
         $role_admin  = Role::where('name', 'admin')->first();
 
         $details = ['bio' => $faker->realText(254), 'profile' => 'https://api.adorable.io/avatars/500/'.str_random(10)];
+        $details_2 = ['bio' => $faker->realText(254), 'profile' => 'https://api.adorable.io/avatars/500/'.str_random(10)];
+        $details_3 = ['bio' => $faker->realText(254), 'profile' => 'https://api.adorable.io/avatars/500/'.str_random(10)];
 
         $client = new User();
         $client->name = 'Client Tester';
         $client->email = 'client@example.com';
-        $client->password = bcrypt('secret');
+        $client->password = bcrypt('test1');
         $client->save();
         $client->roles()->attach($role_client);
         $client->details()->create($details);
@@ -32,17 +34,17 @@ class UserTableSeeder extends Seeder
         $volunteer = new User();
         $volunteer->name = 'Volunteer Tester';
         $volunteer->email = 'volunteer@example.com';
-        $volunteer->password = bcrypt('secret');
+        $volunteer->password = bcrypt('test2');
         $volunteer->save();
         $volunteer->roles()->attach($role_volunteer);
-        $volunteer->details()->create($details);
+        $volunteer->details()->create($details_2);
 
         $admin = new User();
         $admin->name = 'Admin Tester';
         $admin->email = 'admin@example.com';
-        $admin->password = bcrypt('secret');
+        $admin->password = bcrypt('test3');
         $admin->save();
         $admin->roles()->attach($role_admin);
-        $admin->details()->create($details);
+        $admin->details()->create($details_3);
     }
 }
